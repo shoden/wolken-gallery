a478453a598b9c52c948e5e74846b00a04b47766
Juan Roldan Ruiz <juan.roldan@gmail.com>
2011-04-01 13:50:51 +0200

    login

--- backend/lang.php
+++ backend/lang.php
@@ -1,23 +1,18 @@
 <?php
-// DEFAULT LANGUAGE FILE PHPMYLOGON
-// English
+$lang['sessionproblem']		= "Hubo un error al iniciar la sesi&oacute;n PHP";
+$lang['functionproblem']	= "Llamada a la funci&oacute;n no es correcta.";
+$lang['selectdberror']	= "<div class=\"error\">Error al conectar con la base de datos.</div>";
+$lang['connecterror']		= "<div class=\"error\">Error al conectar con la base de datos.</div>";
 
-$lang['sessionproblem']		= "There is a problem with the sessions from PHP. Please make sure 'session_start();' is placed on the top of the page where you use PhpMyLogon!";
-$lang['functionproblem']	= "You didn't use the function as it should. Please read the readme or the text which describes the function before using it. Check if you use the right variables and names.";
-$lang['selectdberror']		= "<span style='color: red;'>There was an error while selecting the database. Please contact the webmaster (database name incorrect).</span>";
-$lang['connecterror']		= "<span style='color: red;'>There was an error while connnecting to the MySQL database. Please contact the webmaster (user, password or host incorrect).</span>";
-
-$lang['login-already']		= "You are logged in.";
+$lang['login-already']		= "Ha iniciado sesi&oacute;n.";
 $lang['login-submitbutton']	= "Acceder";
 $lang['login-username']		= "Usuario";
 $lang['login-password']		= "Clave";
 $lang['login-cookie']		= "Recordarme";
-$lang['login-forgotfield']	= "<span style='color: red;'>Debes rellenar todos los campos.</span>";
-$lang['login-notactive']	= "<span style='color: red;'>You're account isn't activated. Please activate your account and try again. Check your inbox for an activation e-mail if you've used the Forgot password-function.</span>";
-$lang['login-incorrect']	= "<span style='color: red;'>La clave introducida para ese usuario no es correcta.</span>";
+$lang['login-forgotfield']	= "<div class=\"error\">Debes rellenar todos los campos.</div>";
+$lang['login-notactive']	= "<div class=\"error\">Su cuenta no est&aacute; activada.</div>";
+$lang['login-incorrect']	= "<div class=\"error\">La clave introducida para ese usuario no es correcta.</div>";
 
-$lang['logout-ok']			= "You're logged out.";
-$lang['logout-nologin']		= "You're not logged in, so there's nothing to log out.";
-
-
+$lang['logout-ok']			= "Ha salido de la sesi&oacute;n.";
+$lang['logout-nologin']		= "Ya ha salido de la sesi&oacute;n.";
 ?>
--- backend/phpmylogon.php
+++ backend/phpmylogon.php
@@ -242,8 +242,21 @@ function pml_login($todo = "",$action = "") {
 		}
 		// Login form
 		?>
-<html>
-<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-es" lang="es-es" dir="ltr">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <title>Galler&iacute;a</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css"/>
+  <link href="../images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+</head>
+<body id="loginbody">
+<div id="logo"></div>
+  <div id="loginwrapper">
+  <div id="loginform">
 		<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
 			<table>
 				<tr>
@@ -264,6 +277,9 @@ function pml_login($todo = "",$action = "") {
 				</tr>
 			</table>
     </form>
+  </div>
+  </div>
+<div id="footer">2011 &copy; Departamento de F&iacute;sica Aplicada. Universidad de C&oacute;rdoba</div>
 </body>
 </html>
 		<?php
